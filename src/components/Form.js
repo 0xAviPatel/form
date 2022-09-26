88e010c93da28dc9903a757dbb4958c634d80cb3
Avi <avip3513@gmail.com>
2022-09-26 23:22:19 +0530

    multiple handler in one function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,50 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [store, setStore] = useState();
-  const [storeSurname, setStoreSurname] = useState();
+  const [name, setName] = useState({
+    fname: "",
+    lname: "",
+  });
 
   const changeHandler = (e) => {
-    setName(e.target.value);
-  };
+    // console.log(e)
 
-  const surnameChangeHandler = (e) => {
-    setSurname(e.target.value);
-  };
+    const val = e.target.value;
+    const name = e.target.name;
 
+    setName((prevVal) => {
+      if (name === "fname") {
+        return { fname: val, lname: prevVal.lname };
+      } else if (name === "lname") {
+        return { fname: prevVal.fname, lname: val };
+      }
+    });
+  };
+  console.log(name);
   function changeName(e) {
     e.preventDefault();
-    setStore(name);
-    setStoreSurname(surname);
   }
-  console.log(name);
+
   return (
     <div>
       <form action="">
         <input
           type="text"
-          value={name}
+          name="fname"
+          value={name.fname}
           placeholder="Enter Name"
           onChange={changeHandler}
         />
         <input
           type="text"
+          name="lname"
           placeholder="Enter surname"
-          value={surname}
-          onChange={surnameChangeHandler}
+          value={name.lname}
+          onChange={changeHandler}
         />
 
         <p>
-          Hello {store} {storeSurname}
+          Hello {name.fname} {name.lname}
         </p>
         <button type="submit" onClick={changeName}>
           Click Me
